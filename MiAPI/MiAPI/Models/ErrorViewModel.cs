5c2d35813d174aa65ce4b60aeff6d9bd3c2f1d24
Jornic <jornic17@gmail.com>
2023-11-15 21:10:27 -0600

    Creacion de proyecto de API

---
+++ MiAPI/MiAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
