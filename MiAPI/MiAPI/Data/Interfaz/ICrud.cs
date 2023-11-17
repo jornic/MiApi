bfd0566a0466236d587b030037cdc851099a16f5
jornic <jornic17@gmail.com>
2023-11-16 20:01:24 -0600

    Contrato

---
+++ MiAPI/MiAPI/Data/Interfaz/ICrud.cs
@@ -0,0 +1,11 @@
+﻿namespace MiAPI.Data.Interfaz
+{
+    public interface ICrud<Entity> where Entity : class
+    {
+        Task<bool> CreateAsync(Entity entity);
+        Task<bool> UpdateAsync(Entity entity);
+        Task<bool> DeleteAsync(int id);
+        Task<Entity> GetByIdAsync(int id);
+        Task<List<Entity>> GetByNameAsync();
+    }
+}
